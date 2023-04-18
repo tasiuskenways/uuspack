b303367ce8b6a3d869d20db6034611bb7f8d452c
Thaufan <sthaufan@gmail.com>
2023-04-18 10:32:01 +0700

    change editor using menu

--- resources/[core]/qb-smallresources/client/editor.lua
+++ resources/[core]/qb-smallresources/client/editor.lua
@@ -1,24 +1,69 @@
-RegisterCommand("record", function()
+RegisterNetEvent("editor:record", function()
     StartRecording(1)
     TriggerEvent('QBCore:Notify', Lang:t('editor.started'), "success")
-end, false)
+end)
 
-RegisterCommand("clip", function()
+RegisterNetEvent("editor:clip", function ()
     StartRecording(0)
-end, false)
+end)
 
-RegisterCommand("saveclip", function()
+RegisterNetEvent("editor:saveclip", function()
     StopRecordingAndSaveClip()
     TriggerEvent('QBCore:Notify', Lang:t('editor.save'), "success")
-end, false)
+end)
 
-RegisterCommand("delclip", function()
+RegisterNetEvent("editor:delclip", function()
     StopRecordingAndDiscardClip()
     TriggerEvent('QBCore:Notify', Lang:t('editor.delete'), "error")
-end, false)
+end)
 
-RegisterCommand("editor", function()
+RegisterNetEvent("editor:editor", function()
     NetworkSessionLeaveSinglePlayer()
     ActivateRockstarEditor()
     TriggerEvent('QBCore:Notify', Lang:t('editor.editor'), "error")
-end, false)
+end)
+
+RegisterCommand('record', function()
+    exports['qb-menu']:openMenu({
+        {
+            header = 'Editor Menu',
+            icon = 'fas fa-video',
+            isMenuHeader = true, -- Set to true to make a nonclickable title
+        },
+        {
+            header = 'Start Record',
+            icon = 'fas fa-circle',
+            params = {
+                event = 'editor:record'
+            }
+        },
+        {
+            header = 'Clip',
+            icon = 'fas fa-clapperboard',
+            params = {
+                event = 'editor:clip'
+            }
+        },
+        {
+            header = 'Save Clip',
+            icon = 'fas fa-floppy-disk',
+            params = {
+                event = 'editor:saveclip'
+            }
+        },
+        {
+            header = 'Delete Clip',
+            icon = 'fas fa-trash',
+            params = {
+                event = 'editor:delclip'
+            }
+        },
+        {
+            header = 'Rockstar Editor',
+            icon = 'fas fa-pen-to-square',
+            params = {
+                event = 'editor:editor'
+            }
+        },
+    })
+end)

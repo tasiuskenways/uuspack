b5c14e16e192f17af245cddc461399e746d3f2b1
TasiusKenways <88521859+tasiuskenways@users.noreply.github.com>
2023-04-18 17:30:12 +0700

    add assets

---
+++ resources/[assets]/uus_assets/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_busmodstr.meta',
+  'mp_f_freemode_01_mp_f_busmodstr.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_busmodstr.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_busmodstr.meta'

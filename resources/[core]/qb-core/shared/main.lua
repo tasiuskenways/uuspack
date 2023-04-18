a3cb214f2c620a518c726a3a85ff68c78293e93d
Thaufan <sthaufan@gmail.com>
2023-04-18 09:48:07 +0700

    Add Food For Starter Item

--- resources/[core]/qb-core/shared/main.lua
+++ resources/[core]/qb-core/shared/main.lua
@@ -6,7 +6,8 @@ local NumberCharset = {}
 QBShared.StarterItems = {
     ['phone'] = { amount = 1, item = 'phone' },
     ['id_card'] = { amount = 1, item = 'id_card' },
-    ['driver_license'] = { amount = 1, item = 'driver_license' },
+    ['tosti'] = { amount = 1, item = "tosti" },
+    ['water_bottle'] = { ammount = 1, item = "water_bottle"}
 }
 
 for i = 48, 57 do NumberCharset[#NumberCharset + 1] = string.char(i) end

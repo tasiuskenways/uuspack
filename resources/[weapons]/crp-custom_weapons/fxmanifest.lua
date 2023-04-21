971f8f263bb0944042a5bf60a2a710d5e096e6c5
Thaufan <sthaufan@gmail.com>
2023-04-21 17:38:08 +0700

    New Weapons

---
+++ resources/[weapons]/crp-custom_weapons/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+---------------------------------------------------------------------------
+-- INCLUDED FILES
+---------------------------------------------------------------------------
+files {
+	'weaponcomponents.meta',
+	'pedpersonality.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weaponglock.meta',
+	'weaponscar.meta',
+	}
+
+---------------------------------------------------------------------------
+-- DATA FILES
+---------------------------------------------------------------------------
+	data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+	data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPONINFO_FILE' 'weaponglock.meta'
+	data_file 'WEAPONINFO_FILE' 'weaponscar.meta'
+	

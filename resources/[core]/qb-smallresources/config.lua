72dc6511df777284638ad5ac776ed6d96badbd2a
Thaufan <sthaufan@gmail.com>
2023-04-23 23:58:36 +0700

    Pillbox Lift

--- resources/[core]/qb-smallresources/config.lua
+++ resources/[core]/qb-smallresources/config.lua
@@ -1,5 +1,6 @@
 Config = {}
-Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
+Config.UseTarget = GetConvar('UseTarget', 'false') ==
+    'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
 -- To make this simple. Everything you need is in the config, except for discord, weapon drops, vehicle classes for cruise, hands up disabled keys, and recoil
 
@@ -14,24 +15,24 @@ Config.AFK = {
 
 Config.Binoculars = {
     fov_max = 70.0,
-    fov_min = 5.0, -- max zoom level (smaller fov is more zoom)
+    fov_min = 5.0,    -- max zoom level (smaller fov is more zoom)
     zoomspeed = 10.0, -- camera zoom speed
-    speed_lr = 8.0, -- speed by which the camera pans left-right
-    speed_ud = 8.0, -- speed by which the camera pans up-down
+    speed_lr = 8.0,   -- speed by which the camera pans left-right
+    speed_ud = 8.0,   -- speed by which the camera pans up-down
     storeBinoclarKey = 177
 }
 
 -- Whether to enable or disable dispatch services
 Config.DispatchServices = {
-    [1] = false, -- Police Vehicles
-    [2] = false, -- Police Helicopters
-    [3] = false, -- Fire Department Vehicles
-    [4] = false, -- Swat Vehicles
-    [5] = false, -- Ambulance Vehicles
-    [6] = false, -- Police Motorcycles
-    [7] = false, -- Police Backup
-    [8] = false, -- Police Roadblocks
-    [9] = false, -- PoliceAutomobileWaitPulledOver
+    [1] = false,  -- Police Vehicles
+    [2] = false,  -- Police Helicopters
+    [3] = false,  -- Fire Department Vehicles
+    [4] = false,  -- Swat Vehicles
+    [5] = false,  -- Ambulance Vehicles
+    [6] = false,  -- Police Motorcycles
+    [7] = false,  -- Police Backup
+    [8] = false,  -- Police Roadblocks
+    [9] = false,  -- PoliceAutomobileWaitPulledOver
     [10] = false, -- PoliceAutomobileWaitCruising
     [11] = false, -- Gang Members
     [12] = false, -- Swat Helicopters
@@ -45,15 +46,15 @@ Config.EnableWantedLevel = false
 
 -- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
 Config.Discord = {
-    ["IsEnabled"] = false, -- If set to true, then discord rich presence will be enabled
-    ["ApplicationId"] = '1043470567325573141', -- The discord application id
-    ["IconLarge"] = 'logo_name', -- The name of the large icon
+    ["IsEnabled"] = false,                                     -- If set to true, then discord rich presence will be enabled
+    ["ApplicationId"] = '1043470567325573141',                 -- The discord application id
+    ["IconLarge"] = 'logo_name',                               -- The name of the large icon
     ["IconLargeHoverText"] = 'This is a Large icon with text', -- The hover text of the large icon
-    ["IconSmall"] = 'small_logo_name', -- The name of the small icon
+    ["IconSmall"] = 'small_logo_name',                         -- The name of the small icon
     ["IconSmallHoverText"] = 'This is a Small icon with text', -- The hover text of the small icon
-    ["UpdateRate"] = 60000, -- How often the player count should be updated
-    ["ShowPlayerCount"] = true, -- If set to true the player count will be displayed in the rich presence
-    ["MaxPlayers"] = 48, -- Maximum amount of players
+    ["UpdateRate"] = 60000,                                    -- How often the player count should be updated
+    ["ShowPlayerCount"] = true,                                -- If set to true the player count will be displayed in the rich presence
+    ["MaxPlayers"] = 48,                                       -- Maximum amount of players
     ["Buttons"] = {
         {
             text = 'First Button!',
@@ -82,21 +83,20 @@ Config.Stun = {
 
 Config.Cruise = 'mp/h'
 Config.IdleCamera = true
-Config.disableHandsupControls = {24, 25, 47, 58, 59, 63, 64, 71, 72, 75, 140, 141, 142, 143, 257, 263, 264}
+Config.disableHandsupControls = { 24, 25, 47, 58, 59, 63, 64, 71, 72, 75, 140, 141, 142, 143, 257, 263, 264 }
 Config.DisableAmbience = false -- Disabled distance sirens, distance car alarms, etc
 Config.HarnessUses = 20
-Config.DamageNeeded = 100.0 -- vehiclepush 0-1000
-Config.EnableProne = false -- prone isnt recomended at this time
-Config.MapText = "UUSPACK" -- This is the name / text shown above the map
+Config.DamageNeeded = 100.0    -- vehiclepush 0-1000
+Config.EnableProne = false     -- prone isnt recomended at this time
+Config.MapText = "UUSPACK"     -- This is the name / text shown above the map
 
 Config.Disable = {
-    disableHudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
-    disableControls = {37}, -- Controls: https://docs.fivem.net/docs/game-references/controls/
-    displayAmmo = true -- false disables ammo display
+    disableHudComponents = { 1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22 }, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
+    disableControls = { 37 },                                            -- Controls: https://docs.fivem.net/docs/game-references/controls/
+    displayAmmo = true                                                   -- false disables ammo display
 }
 
 Config.ConsumablesEat = {
-
     ["sandwich"] = math.random(35, 54),
     ["tosti"] = math.random(40, 50),
     ["twerks_candy"] = math.random(35, 54),
@@ -137,7 +137,7 @@ Config.ConsumablesCustom = {
     --     },
     --     ['replenish'] = {
     --         type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
-    --         replenish = math.random(20, 40), 
+    --         replenish = math.random(20, 40),
     --         isAlcohol = false, -- if you want it to add alcohol count
     --         event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
     --         server = false -- if the event above is a server event
@@ -231,7 +231,7 @@ Config.BlacklistedPeds = {
     [`s_m_y_hwaycop_01`] = true,
 }
 
-Config.HolsterVariant = {130,122,3,6,8}
+Config.HolsterVariant = { 130, 122, 3, 6, 8 }
 Config.HolsterableWeapons = {
     --'WEAPON_STUNGUN',
     'WEAPON_PISTOL',
@@ -246,8 +246,8 @@ Config.HolsterableWeapons = {
 }
 
 Config.Objects = { -- for object removal
-    {coords = vector3(266.09,-349.35,44.74), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02b"},
-    {coords = vector3(285.28,-355.78,45.13), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02a"},
+    { coords = vector3(266.09, -349.35, 44.74), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02b" },
+    { coords = vector3(285.28, -355.78, 45.13), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02a" },
 }
 
 -- You may add more than 2 selections and it will bring up a menu for the player to select which floor be sure to label each section though
@@ -275,7 +275,7 @@ Config.Teleports = {
             ["AllowVehicle"] = false,
             label = false
         },
-        -- [3] = { 
+        -- [3] = {
         --     ['poly'] = {
         --         coords = vector3(0.0, 0.0, 0.0),
         --         heading = 0.0,
@@ -339,6 +339,72 @@ Config.Teleports = {
             label = 'Helipad'
         }
     },
+    -- Pillbox Hospital - 1
+    [3] = {
+        [1] = {
+            ['poly'] = {
+                coords = vector3(335.68, -592.59, 43.27),
+                heading = 68.29,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Main Floor'
+        },
+        [2] = {
+            ['poly'] = {
+                coords = vector3(323.55, -583.27, 28.85),
+                heading = 253.75,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Basement Floor'
+        },
+        [3] = {
+            ['poly'] = {
+                coords = vector3(338.86, -583.94, 74.17),
+                heading = 251.63,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Rooftop Floor'
+        },
+    },
+    -- Pillbox Hospital - 2
+    [4] = {
+        [1] = {
+            ['poly'] = {
+                coords = vector3(336.98, -589.36, 43.27),
+                heading = 68.29,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Main Floor'
+        },
+        [2] = {
+            ['poly'] = {
+                coords = vector3(322.35, -586.66, 28.85),
+                heading = 253.75,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Basement Floor'
+        },
+        [3] = {
+            ['poly'] = {
+                coords = vector3(338.86, -583.94, 74.17),
+                heading = 251.63,
+                length = 2,
+                width = 2
+            },
+            ["AllowVehicle"] = false,
+            label = 'Rooftop Floor'
+        },
+    },
     -- Paleto Police Office
     -- [3] = {
     --     [1] = {
@@ -375,8 +441,9 @@ Config.Teleports = {
 }
 
 Config.DefaultPrice = 20 -- Default price for the carwash
-Config.DirtLevel = 0.1 -- Threshold for the dirt level to be counted as dirty
-Config.CarWash = { -- carwash
+Config.DirtLevel = 0.1   -- Threshold for the dirt level to be counted as dirty
+Config.CarWash = {
+    -- carwash
     [1] = {
         ["label"] = "Hands Free Carwash",
         ["poly"] = {
